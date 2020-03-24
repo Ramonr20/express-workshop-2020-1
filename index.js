284a1c72a5744163e216c2e71757da85d0f2361a
Ramonr20 <hermon.rios@outlook.com>
2020-03-23 20:52:49 -0600

    pokedex.json added, routes for search pokemon added

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 const express = require('express');
 const app = express();
+const { pokemon } = require('./pokedex.json');
 
 app.get("/", (req, res, next) => {
-    res.send("Welcome to fsocity");
+    res.status(200);
+    res.send("Bienvenido al Pokedex");
 });
 
 // ('url', (req, res, next) => {})
-app.get('/:name', (req, res, next) => {
-    res.status(200);
-    res.send("Hola, " + req.params.name);
+app.get('/pokemon/all/', (req, res, next) => {
+    res.send(pokemon);
 });
+
+app.get('/pokemon/:id([0-9]{1,3})/', (req, res, next) => {
+    const id = req.params.id - 1;
+    if (id >= 0 && id <= 150) {
+        res.status(200);
+        res.send(pokemon[id]);
+    } else {
+        res.status(404);
+        res.send("Pokemon no encontrado");
+    }
+});
+
+app.get('/pokemon/:name', (req, res, next) => {
+    const name = req.params.name;
+
+    pokemon.forEach(pok => {
+        if (pok.name == name) {
+            res.status(200);
+            return res.send(pok);
+        }
+    });
+    res.status(404);
+    res.send("Pokemon no encontrado");
+});
+
 // process.env.PORT -> define the 80 port if it's no used and not declared
 app.listen(process.env.PORT || 3000, () => {
 
