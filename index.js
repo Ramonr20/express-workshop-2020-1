e4c4aeb1288cfd685933afa15c90e52b0b234768
Ramonr20 <hermon.rios@outlook.com>
2020-03-27 17:11:53 -0600

    Refactoring get methods using filter and ternary operator

--- index.js
+++ index.js
@@ -1,39 +1,48 @@
 const express = require('express');
 const app = express();
-const { pokemon } = require('./pokedex.json');
+const { pokemon } = require('./pokedex.json'); //Destructuring
+
+
+/* 
+GET - obtener recursos
+POST - almacenar/crear recursos
+PATCH - modificar una parte de un recurso
+PUT - modificar un recurso
+DELETE - borrar un recurso
+*/
 
 app.get("/", (req, res, next) => {
-    res.status(200);
-    res.send("Bienvenido al Pokedex");
+    return res.status(200).send("Bienvenido al Pokedex");
 });
 
 // ('url', (req, res, next) => {})
-app.get('/pokemon/all/', (req, res, next) => {
-    res.send(pokemon);
+app.get('/pokemon/', (req, res, next) => {
+    return res.status(200).send(pokemon);
 });
 
 app.get('/pokemon/:id([0-9]{1,3})/', (req, res, next) => {
     const id = req.params.id - 1;
+
     if (id >= 0 && id <= 150) {
-        res.status(200);
-        res.send(pokemon[id]);
-    } else {
-        res.status(404);
-        res.send("Pokemon no encontrado");
+        return res.status(200).send(pokemon[id]);
     }
+
+    return res.status(200).send("Pokemon no encontrado");
 });
 
-app.get('/pokemon/:name', (req, res, next) => {
+app.get('/pokemon/:name([A-Za-z]+)/', (req, res, next) => {
     const name = req.params.name;
 
-    pokemon.forEach(pok => {
-        if (pok.name == name) {
-            res.status(200);
-            return res.send(pok);
-        }
+    const poke = pokemon.filter((p) => {
+        return (p.name.toLowerCase() == name.toLowerCase()) ? p : null;
     });
-    res.status(404);
-    res.send("Pokemon no encontrado");
+    
+    if (poke.length != null) {
+        return res.status(200).send(poke);
+    }
+
+    return res.status(404).send("Pokemon no encontrado")
+
 });
 
 // process.env.PORT -> define the 80 port if it's no used and not declared
