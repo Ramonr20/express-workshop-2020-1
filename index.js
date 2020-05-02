1cad093e2126c7b7f980ca2ddfb540d088f196da
Ramonr20 <hermon.rios@outlook.com>
2020-05-02 10:46:18 -0500

    user rout added and get and post routes for user were created

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 
 const pokemon = require('./routes/pokemon');
+const user = require('./routes/user');
 
 app.use(morgan('dev'));
 app.use(express.json());
@@ -23,6 +24,7 @@ app.get("/", (req, res, next) => {
 
 // cargar las rutas de pokemon
 app.use("/pokemon", pokemon);
+app.use("/user", user);
 
 app.use((req, res, next) => {
     return res.status(404).json({code: 404, message: "URL no encontrada"});
